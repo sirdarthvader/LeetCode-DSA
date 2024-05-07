fe54277327705f1dd94883fc409641180e56d87b
Ashish Singh <ashishcodes4@gmail.com>
2024-05-07 23:24:53 +0530

    SinglyLinkedList
    - Theory & questions on SinglyLinkedList

---
+++ LinkedList/SinglyLikedList/index.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.next = null;
+    this.length = null;
+  }
+}
