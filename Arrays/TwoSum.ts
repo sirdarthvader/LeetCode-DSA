3f3627b97614ada65c2d633eead0a5fd44b2fd07
Ashish Singh <ashishcodes4@gmail.com>
2024-05-08 20:09:20 +0530

    Arrays 2 Sums

--- Arrays/TwoSum.ts
+++ Arrays/TwoSum.ts
@@ -19,13 +19,13 @@ function twoSum(nums: number[], target: number) {
   const map = {};
 
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
+    const complement = target - nums[i]; // 4,
 
     if (map[complement] !== undefined) {
-      return [map[complement], i];
+      return [map[complement], i]; // [2, 4]
     }
 
-    map[nums[i]] = i;
+    map[nums[i]] = i; // {3: 0, 2: 1, 4: 2, 5: 3 }
   }
 
   return null; // If no solution found
@@ -33,5 +33,5 @@ function twoSum(nums: number[], target: number) {
 
 // Test cases
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
-console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+console.log(twoSum([3, 2, 4, 5, 7, 9], 11)); // Output: [2, 4]
 console.log(twoSum([3, 3], 6)); // Output: [0, 1]
