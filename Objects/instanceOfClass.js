01ac8d9ca1b2d9bae40bfab0e689deef76346316
Ashish Singh <ashishcodes4@gmail.com>
2024-05-11 01:04:00 +0530

    Update instanceOfClass
    - update best solution

--- Objects/instanceOfClass.js
+++ Objects/instanceOfClass.js
@@ -1,4 +1,4 @@
-// // 2618. Check if Object Instance of Class
+// 2618. Check if Object Instance of Class
 // Write a function that checks if a given value is an instance of a given class or superclass. For this problem, an object is considered an instance of a given class if that object has access to that class's methods.
 
 // There are no constraints on the data types that can be passed to the function. For example, the value or the class could be undefined.
@@ -70,3 +70,11 @@ var checkIfInstanceOf = function (obj, classFunction) {
 /**
  * checkIfInstanceOf(new Date(), Date); // true
  */
+
+// Best Solution
+
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined || typeof classFunction !== "function")
+    return false;
+  return Object(obj) instanceof classFunction;
+};
